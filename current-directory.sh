3028522fe60cc7bbf2138116410297c796e4a518
Vibhakar Kumar <102617175+vibhakar246@users.noreply.github.com>
2026-03-03 23:16:37 +0530

    Update current-directory.sh

--- current-directory.sh
+++ current-directory.sh
@@ -7,8 +7,6 @@ CURRENT_DIR=$(pwd)
 echo "Current Directory: $CURRENT_DIR"
 
 echo "Saving directory to file..."
-
-sudo mkdir -p /opt
 echo "Current Directory: $CURRENT_DIR" | sudo tee /opt/current-directory.txt > /dev/null
 
 echo "File saved at /opt/current-directory.txt"
